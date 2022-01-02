fadeaf1f7b0cd86fbcaaa8d96a3269c4a62595e9
Zamir Musaev <zamir.ru@gmail.com>
2022-01-03 02:32:19 +0300

    Initial. just structure with toml config and logging

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zamirka/rest-demo
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.4.1
+	github.com/sirupsen/logrus v1.8.1
+)
---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,39 @@
+package apiserver
+
+import "github.com/sirupsen/logrus"
+
+// APIServer ...
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+}
+
+// New ...
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+	}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("Starting API server")
+
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
